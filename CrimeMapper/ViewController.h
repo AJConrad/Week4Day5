20956d66dd0f09490d4ebc9124b84dd58e9cc331
Andrew Conrad <Andrew@andrews-macbook.apollosrc.root>
2016-05-05 14:34:36 -0400

    Initial Commit

---
+++ CrimeMapper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrimeMapper
+//
+//  Created by Andrew Conrad on 5/5/16.
+//  Copyright © 2016 AndrewConrad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CrimeMapper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CrimeMapper
+//
+//  Created by Andrew Conrad on 5/5/16.
+//  Copyright © 2016 AndrewConrad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
